a468852107422b7e59f4088b7238b8f066bc93a8
Amira Corine <amiraherring4@gmail.com>
2026-09-20 17:11:02 -0400

    Update find_homologs.sh with clean outfmt

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 QUERY=$1
 SUBJECT=$2
 EVALUE=$3
 OUTFILE=$4
 
-tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" | awk '$3 > 30 && $4 > 90 {print $2}' | sort -u > "$OUTFILE"
+# Run tblastn and write output to OUTFILE
+tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt 6 -out "$OUTFILE"
