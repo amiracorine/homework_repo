10747b922ef4554c2e1a997b0951cb73c6677942
Amira Corine <amiraherring4@gmail.com>
2026-09-20 17:11:08 -0400

    Revert "Update find_homologs.sh with clean outfmt"
    This reverts commit a468852107422b7e59f4088b7238b8f066bc93a8.

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 QUERY=$1
 SUBJECT=$2
 EVALUE=$3
 OUTFILE=$4
 
-# Run tblastn and write output to OUTFILE
-tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt 6 -out "$OUTFILE"
+tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" | awk '$3 > 30 && $4 > 90 {print $2}' | sort -u > "$OUTFILE"
