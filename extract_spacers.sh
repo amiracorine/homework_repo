7698e127a33340ac01448c6b5aa93ed78dc9f94b
Amira Corine <amiraherring4@gmail.com>
2026-09-19 20:52:21 -0400

    Add extract_spacers.sh extra credit script

---
+++ extract_spacers.sh
@@ -0,0 +1,15 @@
+REPEAT_QUERY=$1
+ASSEMBLY=$2
+OUTPUT=$3
+
+makeblastdb -in "$ASSEMBLY" -dbtype nucl -out temp_assembly_db > /dev/null 2>&1
+
+blastn -query "$REPEAT_QUERY" -db temp_assembly_db -outfmt "6 sseqid sstart send" | awk '$2 > $3 {tmp=$2; $2=$3; $3=tmp} {print $1, $2, $3}' | sort -k1,1 -k2,2n > repeats.txt
+
+tail -n +2 repeats.txt > next_repeats.txt
+
+paste repeats.txt next_repeats.txt | awk '$1 == $4 && $6 > $3 { print $1, $3, $6 - 1 }' | tr ' ' '\t' > spacers.bed
+
+seqtk subseq "$ASSEMBLY" spacers.bed > "$OUTPUT"
+
+rm -f temp_assembly_db* repeats.txt next_repeats.txt spacers.bed
