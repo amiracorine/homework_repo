4c723647a4dd06afff9f1384c47b101f0c62292f
Amira Corine <amiraherring4@gmail.com>
2026-09-20 17:15:44 -0400

    Update find_homologs.sh with blast tabular filtering

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 QUERY=$1
 SUBJECT=$2
 EVALUE=$3
 OUTFILE=$4
 
-tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" | awk '$3 > 30 && $4 > 90 {print $2}' | sort -u > "$OUTFILE"
+tblastn -query "$QUERY" -subject "$SUBJECT" -evalue "$EVALUE" -outfmt 6 | \
+awk '$3 >= 30 && $4 >= 100' > "$OUTFILE"
